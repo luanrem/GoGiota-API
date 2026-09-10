f0944307445f7568c1335e626d043df639451504
luanrem <luanrem@gmail.com>
2026-09-10 18:23:25 +0200

    create api server and serve it

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/luanrem/GoGiota-API/internal/api"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("GOGIOTA_DATABASE_USER"),
+		os.Getenv("GOGIOTA_DATABASE_PASSWORD"),
+		os.Getenv("GOGIOTA_DATABASE_HOST"),
+		os.Getenv("GOGIOTA_DATABASE_PORT"),
+		os.Getenv("GOGIOTA_DATABASE_NAME"),
+	))
+	if err != nil {
+		panic(err) // o erro do pgxpool.New não pega se o db está no ar, só se está malformada
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	api := api.Api{
+		Router: chi.NewMux(),
+	}
+
+	fmt.Println("Server is running on :3080")
+	if err := http.ListenAndServe("localhost:3080", api.Router); err != nil {
+		panic(err)
+	}
+
 }
