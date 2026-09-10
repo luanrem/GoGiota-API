2a77974eefc4c6da8b254e4a7bbd1e95fd9d1d6a
luanrem <luanrem@gmail.com>
2026-09-10 17:23:15 +0200

    organize db and create first migration

---
+++ internal/store/pgstore/migrations/001_create_pessoa_fisica.sql
@@ -0,0 +1,20 @@
+-- Write your migrate up statements here
+
+create table pessoa_fisica (
+  id            integer       generated always as identity primary key,
+  renda_mensal  numeric(15,2) not null,
+  idade         integer       not null,
+  nome_completo varchar(255)  not null,
+  celular       varchar(20)   not null,
+  email         varchar(255)  not null,
+  categoria     varchar(50)   not null,
+  saldo         numeric(15,2) not null default 0,
+
+  constraint pessoa_fisica_saldo_nao_negativo check (saldo >= 0),
+  constraint pessoa_fisica_renda_nao_negativa check (renda_mensal >= 0),
+  constraint pessoa_fisica_com_idade_dentro_do_limite check (idade >= 0 and idade <= 150)
+);
+
+---- create above / drop below ----
+
+drop table pessoa_fisica;
